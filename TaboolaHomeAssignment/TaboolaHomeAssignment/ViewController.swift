0b26e67a33c17ba2b0a341f261c30063faa95f12
alonhrri <alonhrri@gmail.com>
2019-04-20 17:22:51 +0300

    Home Assignment Project

---
+++ TaboolaHomeAssignment/TaboolaHomeAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaboolaHomeAssignment
+//
+//  Created by Alon Harari on 20/04/2019.
+//  Copyright © 2019 Alon Harari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
